800516643d40535b3b94f53fcef26b64b9690f22
MateTheBrogrammer <137646488+MateTheBrogrammer@users.noreply.github.com>
2023-07-08 19:53:43 +0400

    Delete Web Files directory

--- Web Files/function.js
+++
@@ -1,8 +0,0 @@
-const shareButton = document.getElementById("shareButton");
-const clickedDisplay = document.getElementById("clickedDisplay");
-const activeButton = document.getElementById('flexDisplayShare');
-
-shareButton.addEventListener('click', function() {
-    clickedDisplay.style.display = clickedDisplay.style.display === 'none' ? 'block' : 'none';
-});
-
